6b13668cbe111cdc1b25989b2f4c6f54d02c1c7a
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-03-24 20:01:02 +0530

    Time: 78 ms (32.48%) | Memory: 126.6 MB (59.18%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& M) {
+        if (M.empty()) return days;
+
         sort(M.begin(), M.end());
 
-        int res = 0;
+        int res = M[0][0] - 1;
 
-        int S = M[0][0];
         int E = M[0][1];
-
-        if(S > 1){
-            res += S - 1;
-        }
         
-
         for(int i = 1; i < M.size(); i++){
             int currS = M[i][0];
             int currE = M[i][1]; 
@@ -21,9 +17,8 @@ public:
                 if(currE > E){
                     E = currE;
                 }
-            }else if(currS > E + 1){
+            } else {
                 res += currS - E - 1 ;
-                S = currS;
                 E = currE;
             }
 
